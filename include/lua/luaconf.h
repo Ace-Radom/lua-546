2735ae91388af64485309713e3d6c97aa5de49da
Ace_Radom <sichenradomlyu@gmail.com>
2023-12-13 16:04:22 +0100

    add LUA_CDIR\?.luac to LUA_PATH_DEFAULT

--- include/lua/luaconf.h
+++ include/lua/luaconf.h
@@ -211,7 +211,8 @@
 		LUA_LDIR"?.lua;"  LUA_LDIR"?\\init.lua;" \
 		LUA_CDIR"?.lua;"  LUA_CDIR"?\\init.lua;" \
 		LUA_SHRDIR"?.lua;" LUA_SHRDIR"?\\init.lua;" \
-		".\\?.lua;" ".\\?\\init.lua;" ".\\?.luac"
+		".\\?.lua;" ".\\?\\init.lua;" \
+		".\\?.luac;" LUA_CDIR"?.luac"
 #endif
 
 #if !defined(LUA_CPATH_DEFAULT)
